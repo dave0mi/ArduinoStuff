0081b334f1aa2dcc01f7787ab845324f48c58278
Dave Ilstrup <dave0mi@gmail.com>
2016-04-30 15:42:44 -0700

    added BPM detector from light based sensor ...

--- CondenserMic/CondenserMic.ino
+++ CondenserMic/CondenserMic.ino
@@ -1,31 +1,16 @@
 /*
-  Analog Input
- Demonstrates analog input by reading an analog sensor on analog pin 0 and
- turning on and off a light emitting diode(LED)  connected to digital pin 13.
- The amount of time the LED will be on and off depends on
- the value obtained by analogRead().
+  Based on: "Analog Input"
 
- The circuit:
- * Potentiometer attached to analog input 0
- * center pin of the potentiometer to the analog pin
- * one side pin (either one) to ground
- * the other side pin to +5V
- * LED anode (long leg) attached to digital output 13
- * LED cathode (short leg) attached to ground
-
- * Note: because most Arduinos have a built-in LED attached
- to pin 13 on the board, the LED is optional.
-
-
- Created by David Cuartielles
- modified 30 Aug 2011
- By Tom Igoe
+  Created by David Cuartielles
+  modified 30 Aug 2011
+  By Tom Igoe
+  modified 30 Apr 2016
+  By Dave Ilstrup
 
  This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/AnalogInput
-
  */
+
+int gAttack, gDecay, gThreshold;
 
 #define COEF 0.5
 #define MEAN_VALUE 340
@@ -51,7 +36,7 @@ void FindExtremaOverWindow(uint16_t* pmax, int* maxIdx, uint16_t* pmin, int* min
 
 void setup() {
   // declare the ledPin as an OUTPUT:
-  //pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   Serial.begin(115200);
 
   // initialize state variables for timing, sampling ...
@@ -66,6 +51,101 @@ void setup() {
 
   // initialize extrema indecies ...
   maxOver10Idx=minOver10Idx=maxOver100Idx=minOver100Idx=0;
+}
+
+
+//
+// The original code used different constants because
+// of the input range of that sensor.
+//
+static int gZeroBias = 340;
+static int IBI = 60;
+static int BPM = 60;
+static boolean Pulse = false;
+
+// return non-zero if the BPM variable has been updated ...
+int UpdateBeatFollower(uint16_t Signal) {
+
+static int rate[10];                    // used to hold last ten IBI values
+static unsigned long sampleCounter = 0; // used to determine pulse timing
+static unsigned long lastBeatTime = 0;  // used to find the inter beat interval
+static int P = gZeroBias;               // used to find peak in pulse wave
+static int T = gZeroBias;               // used to find trough in pulse wave
+static int thresh = gZeroBias;          // used to find instant moment of heart beat
+static int amp = 400;                   // used to hold amplitude of pulse waveform
+static boolean firstBeat = true;        // used to seed rate array so we startup with reasonable BPM
+static boolean secondBeat = true;       // used to seed rate array so we startup with reasonable BPM
+
+    sampleCounter += 2;                         // keep track of the time in mS with this variable
+    int N = sampleCounter - lastBeatTime;       // monitor the time since the last beat to avoid noise
+    int ret = 0; // default is no update ...
+
+//  find the peak and trough of the pulse wave
+    if(Signal < thresh && N > (IBI/5)*3){       // avoid dichrotic noise by waiting 3/5 of last IBI
+        if (Signal < T){                        // T is the trough
+            T = Signal;                         // keep track of lowest point in pulse wave 
+         }
+       }
+      
+    if(Signal > thresh && Signal > P){          // thresh condition helps avoid noise
+        P = Signal;                             // P is the peak
+       }                                        // keep track of highest point in pulse wave
+    
+  //  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
+  // signal surges up in value every time there is a pulse
+  if (N > 250){                                   // avoid high frequency noise
+   if ( (Signal > thresh) && (Pulse == false) && (N > (IBI/5)*3) ){        
+    Pulse = true;                               // set the Pulse flag when we think there is a pulse
+    digitalWrite(ledPin,HIGH);                // turn on pin 13 LED
+    IBI = sampleCounter - lastBeatTime;         // measure time between beats in mS
+    lastBeatTime = sampleCounter;               // keep track of time for next pulse
+         
+         if(firstBeat){                         // if it's the first time we found a beat, if firstBeat == TRUE
+             firstBeat = false;                 // clear firstBeat flag
+             return ret;                        // IBI value is unreliable so discard it
+            }   
+         if(secondBeat){                        // if this is the second beat, if secondBeat == TRUE
+            secondBeat = false;                 // clear secondBeat flag
+               for(int i=0; i<=9; i++){         // seed the running total to get a realisitic BPM at startup
+                    rate[i] = IBI;                      
+                    }
+            }
+          
+    // keep a running total of the last 10 IBI values
+    word runningTotal = 0;                   // clear the runningTotal variable    
+
+    for(int i=0; i<=8; i++){                // shift data in the rate array
+          rate[i] = rate[i+1];              // and drop the oldest IBI value 
+          runningTotal += rate[i];          // add up the 9 oldest IBI values
+        }
+        
+    rate[9] = IBI;                          // add the latest IBI to the rate array
+    runningTotal += rate[9];                // add the latest IBI to runningTotal
+    runningTotal /= 10;                     // average the last 10 IBI values 
+    BPM = 60000/runningTotal;               // how many beats can fit into a minute? that's BPM!
+    ret = 1; // new BPM ...
+   }                       
+  }
+
+  if (Signal < thresh && Pulse == true){    // when the values are going down, the beat is over
+      digitalWrite(ledPin,LOW);             // turn off pin 13 LED
+      Pulse = false;                        // reset the Pulse flag so we can do it again
+      amp = P - T;                          // get amplitude of the pulse wave
+      thresh = amp/2 + T;                   // set thresh at 50% of the amplitude
+      P = thresh;                           // reset these for next time
+      T = thresh;
+     }
+  
+  if (N > 2500){                            // if 2.5 seconds go by without a beat
+      thresh = gZeroBias;                   // set thresh default
+      P = gZeroBias;                        // set P default
+      T = gZeroBias;                        // set T default
+      lastBeatTime = sampleCounter;         // bring the lastBeatTime up to date        
+      firstBeat = true;                     // set these to avoid noise
+      secondBeat = true;                    // when we get the heartbeat back
+     }
+
+  return ret;
 }
 
 void loop() {
@@ -85,8 +165,54 @@ void loop() {
   }
 
   if (loopStart_us-prevSampleTime > 2000U) {
-    Serial.print(filteredValue); Serial.print("\n");
+    //Serial.print(filteredValue); Serial.print("\n");
     prevSampleTime = loopStart_us;
+
+	// this is the rate at which the light sensor beat follower runs.
+	if (UpdateBeatFollower(filteredValue)) {
+    		Serial.print(BPM); Serial.print("\n");
+	}
+  }
+
+
+  if (Serial.available()) {
+
+	static int readCnt = 0;
+	static int readState = 0;
+	static String numbuf = ""; 
+
+	char c = Serial.read();
+	//
+	// A is Attack
+	// D is Decay
+	// T is Threshold
+	//
+	if(readCnt == 0) {
+
+		switch(c) {
+		case 'A': case 'D': case 'T':
+			readState = c;
+			readCnt++;
+		break;
+		}
+
+	} else {
+
+		if (c == '\n') {
+			readCnt = 0; // reset parser.
+			int num = numbuf.toInt();
+
+			switch(readState) {
+			case 'A': gAttack = num;    break;
+			case 'D': gDecay = num;     break;
+			case 'T': gThreshold = num; break;
+			}
+
+		} else if (readCnt<8) {
+			numbuf += c;
+			readCnt++;
+		} else readCnt = 0; // messed up, reset...
+	}
   }
 
 #if 0
